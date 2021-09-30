13c351a77c3d3c09c779897fb36b5673e1cb558b
邱緯輔 <a29242628@hotmail.com>
2021-09-30 14:27:22 +0800

    Create new project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 邱緯輔 on 2021/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PraticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraticeApp.swift
+//  Shared
+//
+//  Created by 邱緯輔 on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct PraticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
